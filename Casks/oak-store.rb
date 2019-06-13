2eabdd5c660ffdcb50152119ab9d75d60146a47d
Sergey Nelepa <sergey@oakds.com>
2019-06-14 01:23:54 +0300

    update to rc3

--- Casks/oak-store.rb
+++ Casks/oak-store.rb
@@ -1,6 +1,6 @@
 cask 'oak-store' do
   version '1.0.0-beta-rc3'
-  sha256 '9925714534cb525ca3d5c9559f02cd0dc3dd3ef7b7ff8825cdc134641c3f87c7'
+  sha256 'e991398d3a87493c02cc6d6f6bf0229c0113464e0dfce2d5f40a958e0e173395'
 
   url "https://storage.googleapis.com/oak-releases/Oakstore-darwin-x64_#{version}.zip"
   name 'OAK Store'
