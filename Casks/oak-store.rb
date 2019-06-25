31f5afa206e81dc86c698a40c2cf31d77a1ae112
Sergey Nelepa <sergey@oakds.com>
2019-06-25 20:49:07 +0300

    new server

--- Casks/oak-store.rb
+++ Casks/oak-store.rb
@@ -2,7 +2,7 @@ cask 'oak-store' do
   version '1.0.0-beta-rc4'
   sha256 '65bb353e764f382673cd2e7f4b88cb35cc2502c51915e9815651042d01aae40f'
 
-  url "https://storage.googleapis.com/oak-releases/Oakstore-darwin-x64_#{version}.zip"
+  url "https://metrics.oaknode.com/v1/Oakstore-darwin-x64_#{version}.zip"
   name 'OAK Store'
   homepage 'https://oaknode.com'
 
