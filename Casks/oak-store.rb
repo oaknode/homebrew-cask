f3ad38de5fbeb39bb6775c487c6dea74e38e9d1e
Sergey Nelepa <sergey@oakds.com>
2019-07-26 02:51:24 +0300

    version 0.9.1

--- Casks/oak-store.rb
+++ Casks/oak-store.rb
@@ -1,6 +1,6 @@
 cask 'oak-store' do
-  version '0.9.0'
-  sha256 'b9d95ccf259512801e1f022517303d68712e6e05a553d7813879f82ed954fede'
+  version '0.9.1'
+  sha256 '92346f4c863384044216d80c0ef7efa08f6911f860415e99f073d5cf57aa2454'
 
   url "https://metrics.oaknode.com/v1/oakstore_#{version}.dmg"
   name 'OAK Store'
