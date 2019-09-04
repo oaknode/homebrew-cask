ee45cca682b463dfbd28508c2d5b5763bd8df9cf
oakremi <50513482+oakremi@users.noreply.github.com>
2019-09-04 23:03:19 +0200

    Updated version number

--- Casks/oak-store.rb
+++ Casks/oak-store.rb
@@ -1,8 +1,8 @@
 cask 'oak-store' do
-  version '0.10.1'
+  version '0.19.0'
   sha256 '79e49422064a8e08b344b6e43cc84859ce9686899a0c9e9c18e39ac6532f79fd'
 
-  url "https://metrics.oaknode.com/v1/oakstore_#{version}.dmg"
+  url "https://metrics.oaknode.com/v1/oakstore-#{version}.dmg"
   name 'OAK Store'
   homepage 'https://oaknode.com'
 
